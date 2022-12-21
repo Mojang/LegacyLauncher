afdcc295a8613d1deff9e946e529678fb4c891c2
craftycodie <craftycodie@outlook.com>
2022-12-21 17:43:25 +0000

    Corrected skin adjustments (thanks @DelofJ)

--- src/main/java/net/minecraft/launchwrapper/protocol/SkinURLConnection.java
+++ src/main/java/net/minecraft/launchwrapper/protocol/SkinURLConnection.java
@@ -104,6 +104,12 @@ public class SkinURLConnection extends HttpURLConnection {
         AlphaComposite alpha = AlphaComposite.getInstance(AlphaComposite.SRC_OVER);
         graphics.setComposite(alpha);
 
+        if (tall) {
+            // Flatten second layers.
+            movePart = skin.getSubimage(0, 32, 56, 16);
+            graphics.drawImage(movePart, 0, 16, null);
+        }
+
         if (slim) {
             // Convert alex to steve.
 
@@ -114,38 +120,6 @@ public class SkinURLConnection extends HttpURLConnection {
             graphics.drawImage(movePart, 50, 16, null);
             movePart = skin.getSubimage(53, 20, 2, 12);
             graphics.drawImage(movePart, 54, 20, null);
-
-            if (tall) {
-                // Stretch right sleeve.
-                movePart = skin.getSubimage(45, 32, 9, 16);
-                graphics.drawImage(movePart, 46, 32, null);
-                movePart = skin.getSubimage(49, 32, 2, 4);
-                graphics.drawImage(movePart, 50, 32, null);
-                movePart = skin.getSubimage(53, 36, 2, 12);
-                graphics.drawImage(movePart, 54, 36, null);
-
-                // Stretch left arm.
-                movePart = skin.getSubimage(37, 48, 9, 16);
-                graphics.drawImage(movePart, 38, 48, null);
-                movePart = skin.getSubimage(41, 48, 2, 4);
-                graphics.drawImage(movePart, 42, 32, null);
-                movePart = skin.getSubimage(45, 52, 2, 12);
-                graphics.drawImage(movePart, 46, 36, null);
-
-                // Stretch left sleeve.
-                movePart = skin.getSubimage(53, 48, 9, 16);
-                graphics.drawImage(movePart, 54, 48, null);
-                movePart = skin.getSubimage(57, 48, 2, 4);
-                graphics.drawImage(movePart, 58, 32, null);
-                movePart = skin.getSubimage(61, 52, 2, 12);
-                graphics.drawImage(movePart, 62, 36, null);
-            }
-        }
-
-        if (tall) {
-            // Flatten second layers.
-            movePart = skin.getSubimage(0, 32, 56, 16);
-            graphics.drawImage(movePart, 0, 16, null);
         }
 
         graphics.dispose();
